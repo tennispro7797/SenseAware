e3cbd2ae81279ff4dc55c3c1405d37b61168ce3e
Aditya Mohile <mohileaditya@gmail.com>
2018-06-07 00:25:31 -0400

    Added code to show notification if motion made and screen off

--- app/src/main/java/com/example/adityamohile/senseaware/CheckFlip.java
+++ app/src/main/java/com/example/adityamohile/senseaware/CheckFlip.java
@@ -44,7 +44,7 @@ public class CheckFlip extends Service implements SensorEventListener {
     private final String MY_PREFS_NAME = "SenseAware Prefs";
     private static final int MAKE_CALL_PERMISSION_REQUEST_CODE = 1;
 
-//    final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+    private PowerManager pm;
 
     public CheckFlip() {
     }
@@ -62,6 +62,7 @@ public class CheckFlip extends Service implements SensorEventListener {
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI, new Handler());
+        pm = (PowerManager)getSystemService(Context.POWER_SERVICE);
 
         return START_NOT_STICKY; //START_STICKY makes service run even though app is killed
     }
@@ -111,9 +112,9 @@ public class CheckFlip extends Service implements SensorEventListener {
 
         String phoneNumber = restoredNumber;
 
-//        boolean isScreenOn = pm.isScreenOn();  && isScreenOn
+        boolean isScreenOn = pm.isScreenOn();
 
-        if (!TextUtils.isEmpty(phoneNumber) && !isCallActive(getApplicationContext())) {
+        if (!TextUtils.isEmpty(phoneNumber) && !isCallActive(getApplicationContext()) && isScreenOn) {
             if (checkPermission(Manifest.permission.CALL_PHONE)) {
                 String dial = "tel:" + phoneNumber;
 
@@ -143,8 +144,11 @@ public class CheckFlip extends Service implements SensorEventListener {
             } else {
 //                Toast.makeText(CheckFlip.this, "Permission Call Phone denied", Toast.LENGTH_SHORT).show();
             }
-        } else {
+        } else if (!TextUtils.isEmpty(phoneNumber) && !isCallActive(getApplicationContext()) && !isScreenOn) {
+            showNotification();
 //            Toast.makeText(CheckFlip.this, "Enter a phone number", Toast.LENGTH_SHORT).show();
+        } else {
+
         }
     }
 
@@ -158,17 +162,28 @@ public class CheckFlip extends Service implements SensorEventListener {
         }
     }
 
+    private Intent getNotificationIntent() {
+        Intent intent = new Intent(this,MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        return intent;
+    }
+
     private void showNotification() {
+        Intent callIntent = getNotificationIntent();
+        callIntent.setAction("Make Call");
+        // Have to make sure the pending intent is processed so the call() method can be invoked
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         builder.setSmallIcon(android.R.drawable.ic_input_get);
         Intent intent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         builder.setContentIntent(pendingIntent);
-        builder.setContentTitle("Notifications Title");
+        builder.setContentTitle("Call Motion Registered!");
         builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
-        builder.setContentText("Your notification content here.");
-        builder.setSubText("Tap to view the website.");
-
+        builder.setContentText("The call motion was just registered. Do you want to follow through with this call?");
+        builder.setSubText("Tap the button to call");
+        builder.addAction(R.mipmap.ic_sense_aware_foreground,
+                "Call",PendingIntent.getActivity(this,
+                        0, callIntent, PendingIntent.FLAG_UPDATE_CURRENT));
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         // Will display the notification in the notification bar
